615f4b0571270400e5ccca60a20d0ebf1729371f
Manuel <manuel@b9.cl>
2020-05-25 11:08:25 -0700

    chore: moving index.js inside src

--- index.js
+++
@@ -1,7 +0,0 @@
-import SuggestionListItem from './src/SuggestionListItem';
-import InputAutoSuggest from './src/InputAutoSuggest';
-
-module.exports = {
-    InputAutoSuggest: InputAutoSuggest,
-    SuggestionListItem: SuggestionListItem,
-};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import SuggestionListItem from './SuggestionListItem';
+import InputAutoSuggest from './InputAutoSuggest';
+
+module.exports = {
+    InputAutoSuggest: InputAutoSuggest,
+    SuggestionListItem: SuggestionListItem,
+};
